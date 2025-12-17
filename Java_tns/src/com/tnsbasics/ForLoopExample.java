2a37d0e9c2c2a7f34717364b93a23dccb042a437
M PREETHI RUBY <M PREETHI RUBY@LAPTOP-5FCT0UCC>
2025-12-18 02:20:20 +0530

    commit 1

---
+++ Java_tns/src/com/tnsbasics/ForLoopExample.java
@@ -0,0 +1,19 @@
+package com.tnsbasics;
+
+public class ForLoopExample {
+	
+
+		public static void main(String[] args) {
+			
+			for(int i = 1; i<=1000 ;i++)
+			{
+				System.out.println("Value of i:");
+				System.out.println(i);
+			}
+			
+
+		}
+
+	}
+
+																																							
---
+++ Java_tns/src/com/tnsif/oops/encapsulation/EncapsulationDemo.java
@@ -0,0 +1,17 @@
+package com.tnsif.oops.encapsulation;
+
+public class EncapsulationDemo {
+
+	public static void main(String[] args) {
+//		object creation
+		OopsConceptDemo obj = new OopsConceptDemo () ;
+		obj.setSerialNum(100);
+		obj.setName("Preethi");
+		obj.setAge(20);
+		System.out.println(obj);
+		
+		
+		
+	}
+
+}
